115575413842847aa4881abc661cdefe4f12f721
Dhananjay Jadhav <dhananjayjadhav129@gmail.com>
2025-03-25 23:08:42 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 //  Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //  builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -24,6 +25,9 @@ var app = builder.Build();
 // app.UseHttpsRedirection();
 
 // app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
