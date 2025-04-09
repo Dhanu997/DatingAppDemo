6da7234bbbc32259974c61a0e614919b94ca6220
Dhananjay Jadhav <dhananjayjadhav129@gmail.com>
2025-04-09 17:17:36 +0530

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Sockets;
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -15,7 +16,7 @@ namespace API.Controllers;
 // [Route("api/[controller]")] //api/users
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     //  private readonly DataContext _context = context;
 
@@ -33,5 +34,19 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         var user = await userRepository.GetMemberAsync(username);
         if (user == null) return NotFound();
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null) return BadRequest("No user found in token");
+        var user = await userRepository.GetUserByUsernameAsync(username);
+        if (user == null) return BadRequest("Could not find user");
+
+        mapper.Map(memberUpdateDto, user);
+        if (await userRepository.SaveAllAsync()) return NoContent();
+        return BadRequest("Failed to update user");
     }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,18 +3,19 @@ import { AccountService } from './_services/account.service';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component"
 import { HomeComponent } from './home/home.component';
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   
   private accountService=inject(AccountService);
-  // title = 'Dating App';
+  //title = 'Dating App';
   
 
   ngOnInit(): void {
