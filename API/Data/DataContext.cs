b07c2a731a2dc561ce178c26509117b2923b691c
Dhananjay Jadhav <dhananjayjadhav129@gmail.com>
2025-04-12 00:31:36 +0530

    End of section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -4,17 +4,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-// public class DataContext : DbContext
-// {
-//     public DataContext(DbContextOptions options) : base(options)
-//     {
-//     }
-
-//     protected DataContext()
-//     {
-//     }
-// }
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
+    public DbSet<UserLike> Likes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.Entity<UserLike>()
+            .HasKey(k => new {k.SourceUserId, k.TargetUserId});
+
+        builder.Entity<UserLike>()
+            .HasOne(s => s.SourceUser)
+            .WithMany(l => l.LikedUsers)
+            .HasForeignKey(s => s.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);  // Use .NoAction on a SQL server.
+
+        builder.Entity<UserLike>()
+            .HasOne(s => s.TargetUser)
+            .WithMany(l => l.LikedByUsers)
+            .HasForeignKey(s => s.TargetUserId)
+            .OnDelete(DeleteBehavior.Cascade);  // Use .NoAction on a SQL server.
+
+    }
 }
